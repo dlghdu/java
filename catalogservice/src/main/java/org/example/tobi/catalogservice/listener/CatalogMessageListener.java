3be818a4445677ddc2a606f57cbe6f1dbc0fc0a2
dlghdu <fhrmsl12@gmail.com>
2024-11-25 15:56:43 +0900

    ssss

---
+++ catalogservice/src/main/java/org/example/tobi/catalogservice/listener/CatalogMessageListener.java
@@ -0,0 +1,25 @@
+package org.example.tobi.catalogservice.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.tobi.catalogservice.config.RabbitMQConfig;
+import org.example.tobi.catalogservice.domain.Book;
+import org.example.tobi.catalogservice.domain.BookService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CatalogMessageListener {
+
+    private final BookService bookService;
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void handleProductUpdate(Book book) {
+        log.info("Received product update: {}", book);
+        bookService.addBookToCatalog(book);
+        log.info("Added product to catalog success!");
+    }
+
+}
